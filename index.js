442113c1433b0cb87ccd7c1a0ab5033710a35656
Rasha Baig <rasha.baig@gmail.com>
2018-11-29 11:53:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+var emptyArray = [];  
+for (let i = 0; i < musicians.length; i++) { 
+ instruments
+}
+return emptyArray  
+}
+
