36e457c1d8b2a22c15f08197a920636a90ef390c
Rasha Baig <rasha.baig@gmail.com>
2018-11-29 17:05:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+var emptyArray = [
+  "John Lennon plays guitar"];  
+for (let i = 0; i < musicians.length; i++) { 
+ text(musicians + "plays" + instruments);
+}
+return emptyArray  
+}
+function johnLennonFacts() {
+  return Facts
+}
