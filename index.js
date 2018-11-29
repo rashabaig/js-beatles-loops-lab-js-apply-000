7d4b520b61676a902ee89cd6d552268312acb9ae
Rasha Baig <rasha.baig@gmail.com>
2018-11-29 18:04:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+var emptyArray = [];  
+for (let i = 0; i < musicians.length; i++) { 
+ (musicians[i] + "plays" + instruments[i] );
+ emptyArray.push(musicians[i] + " plays " + instruments[i] );
+}
+return emptyArray  
+}
+function johnLennonFacts(facts) {
+while () 
